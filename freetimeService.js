ea121371823fdfb260f71ac472e1f32d3c9e6fbb
David <31216720+Agnarr43@users.noreply.github.com>
2020-11-20 10:39:26 -0500

    i

--- freetimeService.js
+++ freetimeService.js
@@ -65,7 +65,7 @@ function readHelloMessage(req, res) {
 
 //Checking login details
 function getUsers(req, res, next) {
-    db.many("SELECT ID, username FROM FTUser")
+    db.many("SELECT * FROM FTUser")
         .then(data => {
             res.send(data);
         })
